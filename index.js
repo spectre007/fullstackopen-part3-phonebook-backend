d4dc90fbee33180e1a49a44ed22c063d4d6f1e2c
Alexander Zech <alexzech@gmx.de>
2022-09-14 23:41:27 -0700

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -83,9 +83,11 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((p) => p.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => console.log(error))
 })
 
 app.post("/api/persons", (request, response) => {
