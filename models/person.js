ccd19def92f238891e1dc6b5f07b9741b4f64ab0
Alexander Zech <alexzech@gmx.de>
2022-09-20 22:55:52 -0700

    3.20: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -17,7 +17,17 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-?\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, "Phone number required"]
+  },
 })
 
 personSchema.set("toJSON", {
